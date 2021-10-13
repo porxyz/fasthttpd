743782f20c7fc8fffa585e00dfda84da756f83ee
porxyz <29072624+porxyz@users.noreply.github.com>
2021-10-13 19:09:05 +0300

    Update build.sh
    Translated to python for better portability
    + some improvements

--- build.sh
+++ build.sh
@@ -1,243 +1,303 @@
-#!/usr/bin/php
-<?php
-error_reporting(E_ERROR | E_WARNING);
+#!/usr/bin/env python3
 
-$enable_MOD_MYSQL = true;
-$enable_https = true;
-$debug = false;
+import os
+import sys
 
-$COMPILER = "g++";
-$COMPILER_FLAGS = "-std=c++11 -Wall -Wfatal-errors";
-$LLIBS = "-lpthread";
+enable_MOD_MYSQL = True
+enable_https = True
+debug = False;
 
-
-if($debug)
-	$COMPILER_FLAGS .= " -ggdb -rdynamic";
-else
-	$COMPILER_FLAGS .= " -O3";
+COMPILER = "g++"
+COMPILER_FLAGS = "-std=c++11 -Wall -Wfatal-errors"
+LLIBS = "-lpthread"
 
 
-if($enable_https)
-	$LLIBS.=" -lssl -lcrypto";
-else
-	$COMPILER_FLAGS.=" -DDISABLE_HTTPS";
+if debug:
+	COMPILER_FLAGS += " -ggdb -rdynamic"
+else:
+	COMPILER_FLAGS += " -O3"
+
+
+if enable_https:
+	LLIBS += " -lssl -lcrypto"
+else:
+	COMPILER_FLAGS += " -DDISABLE_HTTPS"
 	
 	
-if($enable_MOD_MYSQL)
-	$LLIBS.=" -lmysqlclient";	
-else
-	$COMPILER_FLAGS.=" -DNO_MOD_MYSQL";
-
-function source_code_modified($source_files,$object_file)
-{
-	$object_last_mod_time = @filemtime($object_file);
+if enable_MOD_MYSQL:
+	LLIBS += " -lmysqlclient"	
+else:
+	COMPILER_FLAGS += " -DNO_MOD_MYSQL"
 	
-	// object does not exist and should be compiled
-	if($object_last_mod_time === FALSE)
-		return TRUE;
+	
 
-	//if a single filename string is given convert it to array; ex: 0 => filename
-	if(!is_array($source_files))
-		$source_files = array($source_files);
-		 
 
-	for($i=0; $i<count($source_files); $i++) // iterate timestamps
-	{
-		// A source code file was modified since last compilation
-		if(filemtime($source_files[$i]) > $object_last_mod_time)
-			return TRUE;
-	}
-
-	return FALSE; // The object is up to date
-}
+def get_source_dependencies(source_file):
+	compiler_output = os.popen(COMPILER + " " + source_file + " -MM").read()
+	compiler_output = compiler_output.replace("\\"," ").replace("\n", " ").replace("\r","")
+	compiler_output = compiler_output[compiler_output.find(": ") + 2 : ]
+	dependencies = compiler_output.split(" ")
+	return [header for header in dependencies if len(header) > 0]
 
 
 
-if(strcasecmp($argv[1],"clean") === 0)
-{
-	$rm_return_value = 0;
-	system("rm -Rf build",$rm_return_value);
-	if($rm_return_value != 0)
-		die("Unable to delete build directory\n");
+
+def source_code_modified(source_file,object_file):
+
+	# object does not exist and should be compiled
+	if not os.path.isfile(object_file):
+		return True
 		
-	exit();
-}
+	object_last_mod_time = os.path.getmtime(object_file)
 
-
-
-$compiler_return_value = 0;
-
-if(count($argv) === 1 or strcasecmp($argv[1],"build") === 0)
-{
-
-	if(!file_exists("build"))
-	{
-		if(mkdir("build") === FALSE)
-			die("Can not create build directory\n");
-	}
-
-	chdir("build");
-
-
-	//compile helper functions
-	if(source_code_modified(array("../helper_functions.h","../helper_functions.cpp"),"helper_functions.o") or strcasecmp($argv[2],"helper_functions") === 0)
-	{
-		echo("Building helper functions\n");
-		system(($COMPILER . " -c ../helper_functions.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile helper functions\n");
-			
-		if(strcasecmp($argv[2],"helper_functions") === 0)
-			exit();
-	}
-
-
-	//compile server config API
-	if(source_code_modified(array("../server_config.h","../server_config.cpp"),"server_config.o") or strcasecmp($argv[2],"server_config") === 0)
-	{
-		echo("Building server configuration API\n");
-		system(($COMPILER . " -c ../server_config.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile server configuration API\n");
-			
-		if(strcasecmp($argv[2],"server_config") === 0)
-			exit();
-	}
-
-	//compile server journal API
-	if(source_code_modified(array("../server_journal.h","../server_journal.cpp"),"server_journal.o") or strcasecmp($argv[2],"server_journal") === 0)
-	{
-		echo("Building server journal API\n");
-		system(($COMPILER . " -c ../server_journal.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile server journal API\n");
-			
-		if(strcasecmp($argv[2],"server_journal") === 0)
-			exit();
-	}
-
-	//compile file permissions API
-	if(source_code_modified(array("../file_permissions.h","../file_permissions.cpp"),"file_permissions.o") or strcasecmp($argv[2],"file_permissions") === 0)
-	{
-		echo("Building file permissions API\n");
-		system(($COMPILER . " -c ../file_permissions.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile file permissions API\n");
-			
-		if(strcasecmp($argv[2],"file_permissions") === 0)
-			exit();
-	}
-
-	//compile http worker
-	if(source_code_modified(array("../http_worker.h","../http_worker.cpp"),"http_worker.o") or strcasecmp($argv[2],"http_worker") === 0)
-	{
-		echo("Building http worker\n");
-		system(($COMPILER . " -c ../http_worker.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile http worker API\n");
-			
-		if(strcasecmp($argv[2],"http_worker") === 0)
-			exit();
-	}
+	source_files = get_source_dependencies(source_file)
+		 
+	for i in range(0,len(source_files)): #iterate timestamps
+		#A source code file was modified since last compilation
+		if os.path.getmtime(source_files[i]) > object_last_mod_time:
+			return True
 	
-	//compile server listener API
-	if(source_code_modified(array("../server_listener.h","../server_listener.cpp"),"server_listener.o") or strcasecmp($argv[2],"server_listener") === 0)
-	{
-		echo("Building server listener API\n");
-		system(($COMPILER . " -c ../server_listener.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile server listener API\n");
-			
-		if(strcasecmp($argv[2],"server_listener") === 0)
-			exit();
-	}
+	return False #The object is up to date
 
-	//compile https listener thread
-	if((source_code_modified(array("../https_listener.h","../https_listener.cpp"),"https_listener.o") or strcasecmp($argv[2],"https_listener") === 0) and $enable_https)
-	{
-		echo("Building https listener thread\n");
-		system(($COMPILER . " -c ../https_listener.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile https listener thread\n");
-			
-		if(strcasecmp($argv[2],"https_listener") === 0)
-			exit();
-	}
+
+
+
+def compile_helper_functions():
+	need_to_build = False
 	
+	if source_code_modified("../helper_functions.cpp","helper_functions.o") and len(sys.argv) < 3:
+		need_to_build = True	
+
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "helper_functions":
+		 need_to_build = True
+		 
+	if need_to_build:
+		print("Building helper functions")
+		compiler_return_value = os.system(COMPILER + " -c ../helper_functions.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile helper functions")
+			exit();
+			
+
+def compile_server_config():
+	need_to_build = False
 	
-
-	//compile MYSQL support module
-	if((source_code_modified(array("../mod_mysql.h","../mod_mysql.cpp"),"mod_mysql.o") or strcasecmp($argv[2],"mod_mysql") === 0) and $enable_MOD_MYSQL)
-	{
-		echo("Building MYSQL support module\n");
-		system(($COMPILER . " -c ../mod_mysql.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile MYSQL support module\n");
+	if source_code_modified("../server_config.cpp","server_config.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "server_config":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building server configuration API")
+		compiler_return_value = os.system(COMPILER + " -c ../server_config.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile server configuration API");
+			exit()
 			
-		if(strcasecmp($argv[2],"mod_mysql") === 0)
-			exit();
-	}
-
-
-	//compile server
-	if(source_code_modified(array("../main.cpp"),"main.o") or strcasecmp($argv[2],"main") === 0)
-	{
-		echo("Building server\n");
-		system(($COMPILER . " -c ../main.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0){die("Can not compile server\n");}
-		if(strcasecmp($argv[2],"main") === 0){exit();}
-	}
-
-
-
-	//code for compiling custom modules start here
-
-
-
-
-	//code for compiling custom modules end here
-
-
-
-	//compile custom code framework
-	if(source_code_modified(array("../custom_bound.h","../custom_bound.cpp"),"custom_bound.o") or strcasecmp($argv[2],"custom_bound") === 0)
-	{
-		echo("Building custom code framework\n");
-		system(($COMPILER . " -c ../custom_bound.cpp " . $COMPILER_FLAGS),$compiler_return_value);
-		if($compiler_return_value != 0)
-			die("Can not compile custom code framework\n");
 			
-		if(strcasecmp($argv[2],"custom_bound") === 0)
-			exit();
-	}
+def compile_server_journal():
+	need_to_build = False
+	
+	if source_code_modified("../server_journal.cpp","server_journal.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "server_journal":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building server journal API")
+		compiler_return_value = os.system(COMPILER + " -c ../server_journal.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile server journal API");
+			exit()
+			
+			
+def compile_file_permissions():
+	need_to_build = False
+	
+	if source_code_modified("../file_permissions.cpp","file_permissions.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "file_permissions":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building file permissions API")
+		compiler_return_value = os.system(COMPILER + " -c ../file_permissions.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile file permissions API");
+			exit()
+			
 
-}
+def compile_http_worker():
+	need_to_build = False
+	
+	if source_code_modified("../http_worker.cpp","http_worker.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "http_worker":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building http worker")
+		compiler_return_value = os.system(COMPILER + " -c ../http_worker.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile http worker API");
+			exit()
+	
+			
+def compile_server_listener():
+	need_to_build = False
+	
+	if source_code_modified("../server_listener.cpp","server_listener.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "server_listener":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building server listener API")
+		compiler_return_value = os.system(COMPILER + " -c ../server_listener.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile server listener API");
+			exit()
+			
+			
+
+def compile_https_listener():
+	need_to_build = False
+	
+	if source_code_modified("../https_listener.cpp","https_listener.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "https_listener":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building https listener API")
+		compiler_return_value = os.system(COMPILER + " -c ../https_listener.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile https listener API");
+			exit()
+	
+			
+def compile_MOD_MYSQL():
+	need_to_build = False
+	
+	if source_code_modified("../mod_mysql.cpp","mod_mysql.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "mod_mysql":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building MYSQL support module")
+		compiler_return_value = os.system(COMPILER + " -c ../mod_mysql.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile MYSQL support module");
+			exit()
+			
+
+def compile_custom_bound():
+	need_to_build = False
+	
+	if source_code_modified("../custom_bound.cpp","custom_bound.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "custom_bound":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building custom code framework")
+		compiler_return_value = os.system(COMPILER + " -c ../custom_bound.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not compile custom code framework");
+			exit()
+
+def compile_main():
+	need_to_build = False
+	
+	if source_code_modified("../main.cpp","main.o") and len(sys.argv) < 3:
+		need_to_build = True
+		
+	if len(sys.argv) >= 3 and sys.argv[1].lower() == "compile" and sys.argv[2].lower() == "main":
+		need_to_build = True
+		
+	if need_to_build:
+		print("Building server")
+		compiler_return_value = os.system(COMPILER + " -c ../main.cpp " + COMPILER_FLAGS)
+		if compiler_return_value != 0:
+			print("Can not build the server");
+			exit()
+
+
+if len(sys.argv) >= 2 and sys.argv[1].lower() == "clean":
+	rm_return_value = os.system("rm -Rf build")
+	if rm_return_value != 0:
+		print("Unable to delete the build directory")
+		
+	exit()
+
+
+
+if not os.path.isdir("build"):
+		try: 
+    			os.mkdir("build",0o755) 
+		except OSError: 
+    			print("Can not create the build directory")
+    			exit()
+    			
+if len(sys.argv) == 1 or (len(sys.argv) >= 2 and sys.argv[1].lower() == "compile"):    		  
+	os.chdir("build")
+
+	compile_helper_functions()
+	compile_server_config()
+	compile_server_journal()
+	compile_file_permissions()
+	compile_http_worker()
+	compile_server_listener()
+
+	if enable_https:
+		compile_https_listener()
+	
+	if enable_MOD_MYSQL:
+		compile_MOD_MYSQL()
+		
+	
+	compile_main()
+	
+	#code for compiling custom modules start here
+
+
+	#code for compiling custom modules end here
+
+	compile_custom_bound()
+
+	os.chdir("..")
+
+
+os.chdir("build")
+all_objects = os.listdir()
+
+should_link = False
+if not os.path.isfile("fasthttpd") or (len(sys.argv) >= 2 and sys.argv[1].lower() == "link"):
+	should_link = True
+else:
+	object_last_mod_time = os.path.getmtime("fasthttpd")
+	for i in range(0,len(all_objects)): 
+		if os.path.getmtime(all_objects[i]) > object_last_mod_time:
+			should_link = True
+			break
+			
+
+if should_link:
+	print("Linking all objects into fasthttpd")
+	compiler_return_value = os.system(COMPILER + " -o fasthttpd *.o " + LLIBS + " " + COMPILER_FLAGS)
+	if compiler_return_value != 0:
+		print("Can not link fasthttpd")
 
 
 
 
-
-$all_objects = array();
-
-$h_folder = opendir(".");
-$filename = "";
-while (false !== ($filename = readdir($h_folder))) 
-{
-	if($filename[strlen($filename) - 1] === 'o' and $filename[strlen($filename) - 2] === '.')
-		$all_objects[] = $filename;
-}
-closedir($h_folder);
-
-
-if(source_code_modified($all_objects,"fasthttpd") or strcasecmp($argv[1],"link") === 0)
-{
-	echo("Linking all objects into fasthttpd\n");
-	system(($COMPILER . " -o fasthttpd *.o " . $LLIBS . " " . $COMPILER_FLAGS),$compiler_return_value);
-	if($compiler_return_value != 0)
-		die("Can not link fasthttpd\n");
-}
-
-
-
-?>
 
