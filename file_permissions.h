77116cac5979956db049a2d914a3a3ffdb3a86e3
porxyz <29072624+porxyz@users.noreply.github.com>
2022-05-28 22:23:06 +0300

    Update file_permissions.h

--- file_permissions.h
+++ file_permissions.h
@@ -2,8 +2,9 @@
 #define __file_permissions_incl__
 
 #include <string>
+#include <cstdint>
 
-int check_file_access(const std::string* filename,const std::string* host_path);
-int measure_host_path_depth(const std::string* filename);
+void init_file_access_control_API(size_t num_workers);
+int check_file_access(size_t worker_id,const std::string& filename,const std::string& host_path);
 
 #endif
